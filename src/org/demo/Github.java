54b13e64ed72da8d5b1af014d43f372ff58c62f5
abuashik1975 <vijayelas22@gmail.com>
2026-01-01 16:15:17 +0530

    projectjava

---
+++ src/org/demo/Github.java
@@ -0,0 +1,13 @@
+package org.demo;
+
+public class Github {
+	
+	public static void main(String[] args) {
+		
+		String s="welcome Git";
+		System.out.println(s);
+		int i =10;
+		System.out.println(i);
+	}
+
+}
